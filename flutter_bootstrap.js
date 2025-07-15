525b575494c8df49d332a6025ab7e27d0f513ab6
aryadiky <aryadiky117@gmail.com>
2025-07-15 11:55:19 +0700

    v2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1267675575"
+    serviceWorkerVersion: "1106458893"
   }
 });
